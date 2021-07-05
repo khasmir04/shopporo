57fa36489d902aceef41934db4233e4112dd5522
Khasmir Jhon Caluscusao <61497334+khasmir04@users.noreply.github.com>
2021-07-05 10:16:51 +0800

    Fixing cors

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import "../styles/antd.less";
 import "../node_modules/slick-carousel/slick/slick.css";
 import "../styles/styles.css";
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
