cf9abdc721a1e3195eec6cc7697789f3755940b6
Robby de Leon <84078050+obideleon@users.noreply.github.com>
2021-07-06 15:12:55 +0800

    Modified ShopQuickView

--- components/shop/ShopQuickView.js
+++ components/shop/ShopQuickView.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
-import Slider from 'react-slick'
-import { Row, Col } from 'antd'
+import React, { useState } from "react";
+import Slider from "react-slick";
+import { Row, Col } from "antd";
 
-import ProductDetailContentOne from '../productDetail/productDetailContent/ProductDetailContentOne'
+import ProductDetailContentOne from "../productDetail/productDetailContent/ProductDetailContentOne";
 
 function ShopQuickView({ data, setModalVisible }) {
   const slider1Settings = {
     arrows: false,
-  }
+  };
   const slider2Settings = {
     arrows: false,
     slidesToShow: 3,
     centerMode: true,
-    centerPadding: '0px',
+    centerPadding: "0px",
     focusOnSelect: true,
     responsive: [
       {
@@ -35,12 +35,12 @@ function ShopQuickView({ data, setModalVisible }) {
         },
       },
     ],
-  }
-  const [nav1, setNav1] = useState()
-  const [nav2, setNav2] = useState()
+  };
+  const [nav1, setNav1] = useState();
+  const [nav2, setNav2] = useState();
   const onAddedToCart = () => {
-    setModalVisible(false)
-  }
+    setModalVisible(false);
+  };
   return (
     /*
     <div className="shop-qv">
@@ -96,11 +96,8 @@ function ShopQuickView({ data, setModalVisible }) {
             src="https://dummyimage.com/400x400"
           />
           <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h2 class="text-sm title-font text-gray-500 tracking-widest">
-              BRAND NAME
-            </h2>
             <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
-              The Catcher in the Rye
+              {data.name}
             </h1>
             <div class="flex mb-4">
               <span class="flex items-center">
@@ -159,7 +156,9 @@ function ShopQuickView({ data, setModalVisible }) {
                 >
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <span class="text-gray-600 ml-3">4 Reviews</span>
+                <span class="text-gray-600 ml-3">
+                  {Math.ceil(Math.random() * 70)} Reviews
+                </span>
               </span>
               <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
                 <a class="text-gray-500">
@@ -201,12 +200,14 @@ function ShopQuickView({ data, setModalVisible }) {
               </span>
             </div>
             <p class="leading-relaxed">
-              Fam locavore kickstarter distillery. Mixtape chillwave tumeric
+              {/* Please remove the other description once data.description in the database already got a value */}
+              {data.description ||
+                `Fam locavore kickstarter distillery. Mixtape chillwave tumeric
               sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
               juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
               seitan poutine tumeric. Gastropub blue bottle austin listicle
               pour-over, neutra jean shorts keytar banjo tattooed umami
-              cardigan.
+              cardigan.`}
             </p>
             <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
               <div class="flex">
@@ -242,10 +243,10 @@ function ShopQuickView({ data, setModalVisible }) {
             </div>
             <div class="flex">
               <span class="title-font font-medium text-2xl text-gray-900">
-                $58.00
+                ${data.discount ? data.price - data.discount : data.price}
               </span>
               <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Button
+                Add to Cart
               </button>
               <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                 <svg
@@ -264,7 +265,7 @@ function ShopQuickView({ data, setModalVisible }) {
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default React.memo(ShopQuickView)
+export default React.memo(ShopQuickView);
