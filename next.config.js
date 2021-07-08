9101a7d7c363a94736fcf1c4a3fe8109c1c5e2ec
Robby de Leon <84078050+obideleon@users.noreply.github.com>
2021-07-08 11:11:55 +0800

    Switched PUBLIC_URL to www.shopporo.ml

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
-const withLess = require('next-with-less');
+const withLess = require('next-with-less')
 
 module.exports = withLess({
   // optional
   env: {
     PUBLIC_URL: 'https://www.shopporo.ml',
-    // PUBLIC_URL: 'http://localhost:3000',
     BACKEND_URL: 'https://api.shopporo.ml',
-    consumerKey: "ck_9f8fc54f84807c9f927975225178ffa345a79c1d",
-    consumerSecret: "cs_0b77b434eb9fde3c54649bcc14cf1c3636dc3c70",
-    sessionSecret: "5EBE2294ECD0E0F08EAB7690D2A6EE69",
+    consumerKey: 'ck_9f8fc54f84807c9f927975225178ffa345a79c1d',
+    consumerSecret: 'cs_0b77b434eb9fde3c54649bcc14cf1c3636dc3c70',
+    sessionSecret: '5EBE2294ECD0E0F08EAB7690D2A6EE69',
   },
   // optional
   lessVarsFilePath: './styles/variables.less',
@@ -20,6 +19,6 @@ module.exports = withLess({
   // Other Config Here...
 
   webpack(config) {
-    return config;
+    return config
   },
-});
+})
